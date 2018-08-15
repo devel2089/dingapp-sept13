fc90d7834ad3b447f0e9e478de0eda8727934f36
devel2089 <devel2089@gmail.com>
2018-08-15 02:04:33 -0400

    hi

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ app.post('/stream', (req, res) => {
             client.query(`DELETE from public."fbai";`)
             /*beginning file upload 
             /*postgres from*/
-            console.log ("hihi"+JSON.stringify(req.files))
+            console.log ("hihi"+JSON.stringify(req.files.fbai))
             if (typeof (req.files['fbai']) != 'undefined' && typeof (req.files['testtable']) != 'undefined') {
             
                 var fileup1 = streamifier.createReadStream(req.files['fbai'].buffer)
