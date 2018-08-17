8f1dc85ae960ec4c33a57ed72fae11cb1edb4e5a
devel2089 <devel2089@gmail.com>
2018-08-17 02:47:37 -0400

    test transaction query

--- app.js
+++ app.js
@@ -223,8 +223,8 @@ app.post('/stream', (req, res) => {
     app.post('/postsql', (req, res) => {
         var postsqlquery = `${req.body.selecta}`
         client.connect();
-        client.query(`delete from transactions`);
-        res.redirect('/sqlquery')
+        client.query(`DELETE FROM public."Transactions";`);
+        res.redirect('/')
 
 
     })
